b09b249d1cd1bd7addfe2463e29ebb837a574946
MegaDen58 <96517069+MegaDen58@users.noreply.github.com>
2022-04-08 06:23:52 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,9 +5,12 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        String result = "";;
         System.out.print("Введите предложение: ");
         String word = in.nextLine();
+        minWordLength(word);
+    }
+    public static void minWordLength(String word){
+        String result = "";;
         int min = word.length();
         String[] words = word.split(" ");
         for(String t : words){
